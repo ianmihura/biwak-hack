b274dbdcd2069d9f710a76d4904b733ebccc3269
ErwinGoneMad <erwin.rodrigues19@gmail.com>
2025-01-15 15:55:57 +0100

    Update harmonic_client.py
    Added function to get companies by ids

--- harmonic_client.py
+++ harmonic_client.py
@@ -19,7 +19,6 @@ async def get_company_info_by_domain(website_domain, api_key):
             print(f"Error getting company description: {e}")
             return None
 
-
 async def get_companies_by_key_words(api_key, key_words: list[str]):
     url = f"https://api.harmonic.ai/search/companies_by_keywords"
     headers = {'apikey': api_key}
@@ -35,20 +34,49 @@ async def get_companies_by_key_words(api_key, key_words: list[str]):
             return None
 
 
+async def get_companies_info_by_ids(ids, api_key):
+    url = "https://api.harmonic.ai/companies/batchGet"
+    body = {
+        "ids": ids
+    }
+    headers = {'apikey': api_key}
+
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.post(url, json=body, headers=headers) as response:
+                response.raise_for_status()
+                return await response.json()
+        except aiohttp.ClientError as e:
+            print(f"Error getting company data: {e}")
+            return None
+
+async def get_companies_info_by_ids(ids, api_key):
+    url = "https://api.harmonic.ai/companies/batchGet"
+    body = {
+        "ids": ids
+    }
+    headers = {'apikey': api_key}
+
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.post(url, json=body, headers=headers) as response:
+                response.raise_for_status()
+                return await response.json()
+        except aiohttp.ClientError as e:
+            print(f"Error getting company data: {e}")
+            return None
+
+
 if __name__ == "__main__":
     async def main():
-        # website_domain = "atomico.com"  # Replace with the actual website domain
-        # api_key = os.getenv("HARMONIC_API_KEY")
-        # company_info = await get_company_info_by_domain(website_domain, api_key)
-
-        companies = await get_companies_by_key_words(os.getenv("HARMONIC_API_KEY"),
-                                             ["content creators", "social media", "brand development", "creator community"])
-        print("companies", companies)
+        website_domain = "atomico.com"  # Replace with the actual website domain
+        api_key = os.getenv("HARMONIC_API_KEY")
+        ids = [3639957, 2261160]
+        companies = await get_companies_info_by_ids(ids, api_key)
 
         if companies:
-            print("Company Information:")
-            print(companies)
-        else:
-            print("Failed to fetch company information.")
+            for company in companies:
+                headcount = company.get('headcount', 0)
+                print(f"Company {company['id']} has {headcount} employees.")
 
     asyncio.run(main())
