ca77c19eaf703945443bef058a2cd4facf94096f
Ian Mihura <ian@oneragtime.com>
2025-01-15 13:05:50 +0100

    more scafolding p.2

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 
 
+def main():
+    # TODO: get company name
+    # TODO: get company description
+
+    name = "Motion Society"
+
+    description = get_company_desc(name)
+    keywords = extract_keyword(description)
+
+    if validate_keywords(keywords):
+        pass
+
+    result = run_query(description, keywords)
+
+    return result
+
+
 def get_company_desc() -> str:
     pass
 
@@ -12,27 +29,42 @@ def validate_keywords(keywords: list[str]) -> bool:
     pass
 
 
-def generate_queries(keywords: list[str]) -> list[str]:
-    # TODO validate queries
+def run_query(description: str, keywords: list[str]) -> dict:
+    # Mayb be recursively called
+    queries = generate_queries(description, keywords)
+    db = {}
+
+    for query in queries:
+        result = call_query(query)
+        shortlist()
+        db = aggr_query_results(db, result)
+    
+    is_valid = validate_db(db)
+
+    if is_valid:
+        return db
+    else:
+        return run_query(description, keywords)
+
+
+def generate_queries(description: str, keywords: list[str]) -> list[str]:
+    # TODO validate queries with description
+    pass
+
+
+def call_query(query: str) -> dict:
+    pass
+
+
+def aggr_query_results(db: dict, result: dict) -> dict:
+    # using global full_db
+    pass
+
+
+def shortlist():
     pass
 
 
 if __name__ == "__main__":
-    # TODO: get company name
-    # TODO: get company description
-
-    name = "Motion Society"
-
-    description = get_company_desc(name)
-    keywords = extract_keyword(description)
-
-    if validate_keywords(keywords):
-        pass
-
-    queries = generate_queries(description, keywords)
-
-    for query in queries:
-        call_query()
-        pass
-
-
+    main()
+    
