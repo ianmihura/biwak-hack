d803e0371e3a1a089265a20ad9938408d0c398a2
Ian Mihura <ian@oneragtime.com>
2025-01-16 09:40:21 +0100

    plug happy path to frontend

--- interface.py
+++ interface.py
@@ -3,6 +3,7 @@ import asyncio
 import streamlit as st
 import random
 from backend import start_backend
+from happy import main as backend
 
 # Application title
 st.markdown("""
@@ -17,20 +18,17 @@ company_name = st.text_input("Enter the company name:", placeholder="E.g., Googl
 def display_competitors(competitors):
     """Displays the list of competitors in a structured table format."""
     st.subheader("Competitors Found:")
-    
+
     # Prepare data for the table
     table_data = []
     for competitor in competitors:
-        table_data.append({
-            "Name": competitor['name'],
-            "Accuracy": competitor['accuracy'],  # Corrected spelling from 'accouracy'
-            "Website": competitor['website'],
-            "Description": competitor['description'],
-            "Headcount": competitor['headcount']
-        })
+        data = {}
+        for c in competitor.keys():
+            data[humanize(c)] = competitor[c]
+        table_data.append(data)
     
     # Sort the table data by accuracy in descending order
-    table_data.sort(key=lambda x: x['Accuracy'], reverse=True)
+    # table_data.sort(key=lambda x: x['accuracy'], reverse=True)
     
     # Display the data as a table
     st.table(table_data)  # Use st.table to display the data in a table format
@@ -41,8 +39,8 @@ if st.button("Analyze Competition", key="analyze"):
         st.write(f"Searching for competitors for **{company_name}**...")
 
         # Call start_backend with the company_name
-        backend_data= asyncio.run(start_backend(company_name)) # Pass company_name as an argument
-        
+        backend_data= asyncio.run(backend(company_name)) # Pass company_name as an argument
+
         if isinstance(backend_data, list):  # Check if backend_data is a list
             st.success("Competitors found!")
             display_competitors(backend_data)  # Use the backend data directly
@@ -51,3 +49,9 @@ if st.button("Analyze Competition", key="analyze"):
             st.warning("Unexpected data format received from the backend.")
     else:
         st.warning("Please enter a company name.")
+
+def humanize(s: str) -> str:
+    if len(s):
+        return s[0].upper() + s[1:]
+    else:
+        return ""
